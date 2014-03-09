1b91f9dabc8b713c9ad87602ce754a7aa4534b52
Fragan <knibals@hotmail.fr>
2014-03-09 20:24:29 +0100

    Formatting code.

--- js/lazyscript.js
+++ js/lazyscript.js
@@ -1,22 +1,4 @@
 /*=== lazyLoading ===*/
-var bLazy;
-
 window.addEventListener('load', function() {
-    bLazy = new Blazy({
-        success: function(elt) {
-            // some stuff
-        },
-        error: function(elt, msg) {
-            console.log("LazyLoad fail.");
-            if (msg === 'invalid')
-                console.log(elt);
-                retry(elt);
-                
-        }
-    });
+   var bLazy = new Blazy();
 }, false);
-
-function retry(elt) {
-    sleep(1000);
-    bLazy.load(elt);
-}
